3b4ec571087c8ce4f97992979caaab0fd6569d87
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-26 00:50:56 -0500

    alpha filter working

--- server/public/scripts/controllers/viewController.js
+++ server/public/scripts/controllers/viewController.js
@@ -1,6 +1,10 @@
-myApp.controller('viewController', ['$scope', '$http', function($scope, $http){
+myApp.controller('viewController', ['$scope', '$http', '$filter', function($scope, $http, $filter){
   console.log('view');
   $scope.pets = [];
+  var filtered = false;
+
+  $scope.alphaFilter = 'Sort Pets Alphabetically';
+
   var getPets = function(){
     $http({
       method:'GET',
@@ -8,9 +12,25 @@ myApp.controller('viewController', ['$scope', '$http', function($scope, $http){
     }).then(function(results){
       console.log('got pets:', results.data);
       $scope.pets = results.data;
+      if (filtered){
+        $scope.pets = $filter('orderBy')($scope.pets, 'name');
+      }
     });//end http.then
   };//end getPets
   getPets();
+
+  $scope.filterPets = function(){
+    if (filtered) {
+      getPets();
+      filtered = false;
+      $scope.alphaFilter = 'Sort Pets Alphabetically';
+    }
+    else {
+      $scope.pets = $filter('orderBy')($scope.pets, 'name');
+      $scope.alphaFilter = 'Unsort'
+      filtered = true;
+    }
+  }
 
   $scope.removePet = function(id){
     console.log('in removePet with', id);
